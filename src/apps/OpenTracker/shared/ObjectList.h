503666f94cb00aa6f6b67e0cecb02904120535b9
Ithamar R. Adema <ithamar@upgrade-android.com>
2014-11-10 09:23:33 +0100

    Add missing argument names, use const
    To make recent GCC versions happy, no functional change.

--- src/apps/OpenTracker/shared/ObjectList.h
+++ src/apps/OpenTracker/shared/ObjectList.h
@@ -668,7 +668,7 @@ BObjectList<T>::BinaryInsert(T *item, CompareFunctionWithState func, void *state
 
 template<class T>
 bool 
-BObjectList<T>::BinaryInsertUnique(T *, CompareFunction func)
+BObjectList<T>::BinaryInsertUnique(T *item, CompareFunction func)
 {
 	int32 index = _PointerList_::BinarySearchIndex(item,
 		(GenericCompareFunction)func);
@@ -681,7 +681,7 @@ BObjectList<T>::BinaryInsertUnique(T *, CompareFunction func)
 
 template<class T>
 bool 
-BObjectList<T>::BinaryInsertUnique(T *, CompareFunctionWithState func, void *state)
+BObjectList<T>::BinaryInsertUnique(T *item, CompareFunctionWithState func, void *state)
 {
 	int32 index = _PointerList_::BinarySearchIndex(item,
 		(GenericCompareFunctionWithState)func, state);
